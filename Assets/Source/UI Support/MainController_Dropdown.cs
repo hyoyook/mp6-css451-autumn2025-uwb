4ff0efd8092034f7d8b36cc8988265548f080514
Alec Situ <epicsitu@gmail.com>
2025-11-22 14:31:30 -0800

    Alec - switching shape behavior Change: Switching shapes now despawns the manipulator as well as resetting internal state to that of: 1. no ctrl pressed 2. No controller selected

--- Assets/Source/UI Support/MainController_Dropdown.cs
+++ Assets/Source/UI Support/MainController_Dropdown.cs
@@ -35,6 +35,7 @@ public partial class MainController : MonoBehaviour
     {
 
         int N, M;
+        HandleDeselection();
         resetState();
         // Debug.Log("OnShapeDropdown called with index: " + index);
         if (index == 0) // 0 = Plane
