154af71018e546864c288fbe6a2c03bc1ca0a1ef
Hyobin Yook <hyobin.april.yook@gmail.com>
2025-11-22 17:53:58 -0800

    Reset Button Implemented

--- Assets/Source/Model/TheWorld.cs
+++ Assets/Source/Model/TheWorld.cs
@@ -1,15 +1,26 @@
+/// ---------------------------------------------------------------------------------
+/// TheWorld.cs
+/// Author: 
+/// Last Edited: November 22, 2025
+/// ---------------------------------------------------------------------------------
+/// Created for MP6, CSS451, UWB. 
+/// 
+/// Manages scene reset
+///
+/// Reference:
+/// * Unity Technologies. (n.d.). "SceneManager.GetActiveScene."
+///   Unity Documentation. https://docs.unity3d.com/6000.2/Documentation/ScriptReference/SceneManagement.SceneManager.GetActiveScene.html
+/// ---------------------------------------------------------------------------------
+
 using UnityEngine;
+using UnityEngine.SceneManagement;   // for reset
 
 public class TheWorld : MonoBehaviour
 {
-    void Start()
-    {
-        // Init Plane and Cylinder 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void onResetButtonClicked()
+    { 
+        var scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex);         // reload the scene
+        Debug.Log("[TheWorld] Reset Scene");
     }
 }
